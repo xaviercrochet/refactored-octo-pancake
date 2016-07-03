1b0acb201c6920f15a2cb8ad17dd8e8ffb3c5a0a
Xavier Crochet <xacrochet@gmail.com>
2016-07-03 17:26:51 +0200

    Added cors support

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ app.listen(process.env.PORT || 3000 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-function allowCrossDomain(req, res, next) {
-  if ('OPTIONS' == req.method) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(cors());
+app.use(cors())
 app.use('/', items);
 app.use('/users', users);
