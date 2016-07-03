ca885e5080cb55f28a83b5e40b4a9b262be3f594
Xavier Crochet <xacrochet@gmail.com>
2016-07-03 17:25:11 +0200

    Added cors support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var items = require('./routes/items');
 var users = require('./routes/users');
 var db = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
 db.connect(process.env.MONGOLAB_RED_URI);
@@ -10,5 +11,19 @@ db.connect(process.env.MONGOLAB_RED_URI);
 app.listen(process.env.PORT || 3000 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+function allowCrossDomain(req, res, next) {
+  if ('OPTIONS' == req.method) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(cors());
 app.use('/', items);
 app.use('/users', users);
